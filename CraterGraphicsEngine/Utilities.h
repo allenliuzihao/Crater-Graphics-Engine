6b10370011b7cc8810eca5268283f3d4bc3fb9b9
allenliuzihao <allenliuzihao@gmail.com>
2020-09-24 13:58:48 -0700

    adding engine constants

---
+++ CraterGraphicsEngine/Utilities.h
@@ -0,0 +1,11 @@
+#ifndef Utilities_h
+#define Utilities_h
+
+#pragma once
+
+// window constants
+const int WINDOW_WIDTH = 1503;
+const int WINDOW_HEIGHT = 903;
+const char* WINDOW_NAME = "Crater Engine";
+
+#endif /* Utilities_h */
--- CraterGraphicsEngine/main.cpp
+++ CraterGraphicsEngine/main.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <stdexcept>
 
+#include "Utilities.h"
+
 int main(int argc, const char * argv[]) {
     glfwInit();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
