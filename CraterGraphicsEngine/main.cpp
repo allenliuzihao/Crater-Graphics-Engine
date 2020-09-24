9fd9450b2aec2f82b758dc8de8d50b841cd0225e
allenliuzihao <allenliuzihao@gmail.com>
2020-09-24 13:54:34 -0700

    getting libraries to load

--- CraterGraphicsEngine/main.cpp
+++ CraterGraphicsEngine/main.cpp
@@ -1,6 +1,27 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <stdexcept>
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
